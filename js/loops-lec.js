680542a7e09dbf75c5a8357db9aaa17cc67fc19a
Daniel <daniel.h.farias2@gmail.com>
2023-02-23 15:55:55 -0600

    message

---
+++ js/loops-lec.js
@@ -0,0 +1,53 @@
+// let i = 0;
+//
+// while (i <=10){
+// 	console.log("while loop iteration #" + i);
+// 	i++;
+// 	// i += 2;
+// 	//1. i + 2
+// 	//2. i = i + 2
+// }
+//
+// let yes = true;
+// while(yes){
+// 	yes = confirm("click okay to confirm");
+// }
+
+//------------ do-while loops notes------------------------------
+// let yes;
+// do{
+// 	yes = confirm("click okay to continue.")
+// }while(yes);
+//
+
+// let y = 10
+// do{
+// 	console.log('while loop iteration #' + y)
+// }while(y < 10)
+
+//-------------for loop----numeric only------------------------------
+// for (init; condition; inc){
+// 	body
+// }
+// for(let i = 1; i <= 10; i++){
+// 	console.log("current iteration: " + i)
+// 	if(i % 2 === 0){
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+//w/continue-----------^^^^^
+//
+// let school = "codeup";
+// for(let i = 0; i < school.length; i++){
+// 	console.log(school.charAt(i))
+// }-------------------^^^ only for strings--------
+
+// for(let i =1; i <= 10; i++){
+// 	console.log("here is a book " + i);
+// 	cont = confirm("click okay to continue");
+// 	if(!cont){
+// 		break;
+// 	}
+// }
+//-----------------example with break----------------------^^^
