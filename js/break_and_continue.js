1ad3b68b8ddc702b5958df70cac6c80cc297e351
Daniel <daniel.h.farias2@gmail.com>
2023-02-26 14:19:28 -0600

    message

---
+++ js/break_and_continue.js
@@ -0,0 +1,15 @@
+let input;
+while(true){
+	input = Number(prompt("add odd number 1 - 50"));
+	if(input % 2 === 1 && input >= 1 && input <= 50) {
+		break;
+	}
+}
+
+for(let a = 1; a <= 50; a += 2){
+	if(a === input){
+		console.log("numbers skip!: " + a);
+		continue;
+	}
+	console.log("this is a odd number " + a);
+}
