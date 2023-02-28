c2073ffdfd00b08f63bd51c1208f7d66734c4581
Daniel <daniel.h.farias2@gmail.com>
2023-02-28 13:26:48 -0600

    math objects lec

---
+++ js/math-object-lec.js
@@ -0,0 +1,18 @@
+function getRandomInt(min, max){
+// 	return Math.random() * (181) + 20;
+}
+// will round decimal to nearest whole number
+console.log(Math.round(6.96));
+
+// will round decimal to whole number downwards
+console.log(Math.floor(6.96));
+//will round decimal to whole number upwards
+console.log(Math.ceil(3.24));
+
+// will return the value of base number to the power of the exponents argument
+// first argument will be base, second will be exponent
+//ex: Math.pow(base, exponent)
+Math.pow(2, 2);
+//will return the square root of the number being passed in as the argument
+console.log(Math.sqrt(81));
+console.log(Math.sqrt(4.56));
