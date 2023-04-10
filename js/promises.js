649dba07d5236ce5a34cadece35abadd54689e01
Daniel <daniel.h.farias2@gmail.com>
2023-04-10 16:32:54 -0500

    promises

---
+++ js/promises.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function getUserLastCommit(username) {
+	let url = `https://api.github.com/users/${username}/events/public`;
+
+	// console.log(url);
+
+	return fetch(url, {headers: {'Authorization': PROMISE_TOKEN}})
+		.then((response) => response.json())
+		.then((listOfEvents)=>{
+			console.log(listOfEvents);
+			for(let event of listOfEvents) {
+				if(event.type ==='PushEvent'){
+					return event.created_at;
+				}
+			}
+		})
+}
+
+console.log(getUserLastCommit('dfarias1'));
+//.then( date => console.log('Last commit date: ', date) );
+document.querySelector('#getCommitDate').addEventListener('click', function(e){
+	e.preventDefault();
+	console.log('clicked');
+	const input = document.querySelector('#username');
+
+	console.log(input.value);
+	getUserLastCommit(input.value).then((date)=>{
+		console.log(date);
+		const div = document.querySelector('#lastcommitdate');
+		div.innerHTML = `Last commit was on ${date}`;
+	});
+})
